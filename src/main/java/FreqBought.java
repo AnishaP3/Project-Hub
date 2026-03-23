89f0c526799c1b231277096846c7f2cbafa8bda7
Mary <mary.pentareddy@ontariotechu.net>
2026-03-22 23:15:34 -0400

    Adding freqBought ShoppingCart.java

--- src/main/java/FreqBought.java
+++ src/main/java/FreqBought.java
@@ -1,9 +1,76 @@
+import java.util.List;
+
 public class FreqBought {
-    // count the number of items added to cart
 
-    //set up the accecories that you may buy according to the item bought
+    /**
+     * Adds a product to the cart with specified quantity
+     * @param product The product to add
+     * @param quantity The quantity to add (default should be 1 from UI)
+     */
+    public static void addToCart(Products product, int quantity) {
+        ShoppingCart.addProduct(product, quantity);
+    }
 
-    // find the images? - suggest on discord.
+    /**
+     * Removes one instance of a product from cart
+     * @param productName The name of the product to remove one of
+     */
+    public static void removeOneFromCart(String productName) {
+        ShoppingCart.removeOneProduct(productName);
+    }
 
-    // make sure the add to cart button works, and then create a button where you can view the items.
+    /**
+     * Removes all instances of a product from cart
+     * @param productName The name of the product to remove completely
+     */
+    public static void removeAllFromCart(String productName) {
+        ShoppingCart.removeAllProducts(productName);
+    }
+
+    /**
+     * Gets the total number of items in cart
+     * @return total item count
+     */
+    public static int getTotalItemCount() {
+        return ShoppingCart.getTotalItemCount();
+    }
+
+    /**
+     * Calculates the total price of all items in cart
+     * @return total price
+     */
+    public static double getTotalPrice() {
+        return ShoppingCart.getTotalPrice();
+    }
+
+    /**
+     * Gets all cart items with their details
+     * @return List of CartItem objects
+     */
+    public static List<ShoppingCart.CartItem> getCartItems() {
+        return ShoppingCart.getCartItems();
+    }
+
+    /**
+     * Clears the entire cart
+     */
+    public static void clearCart() {
+        ShoppingCart.clearCart();
+    }
+
+    /**
+     * Checks if cart is empty
+     * @return true if empty
+     */
+    public static boolean isCartEmpty() {
+        return ShoppingCart.isEmpty();
+    }
+
+    /**
+     * Gets a formatted summary for debugging
+     * @return Cart summary string
+     */
+    public static String getCartSummary() {
+        return ShoppingCart.getCartSummary();
+    }
 }
