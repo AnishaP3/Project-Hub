3d47d75d8bab04e329a2ec169bf36ad2335d0933
Mary <mary.pentareddy@ontariotechu.net>
2026-03-22 22:47:01 -0400

    Temp boiler plate code for FreqBought.java

---
+++ src/main/java/FreqBought.java
@@ -0,0 +1,9 @@
+public class FreqBought {
+    // count the number of items added to cart
+
+    //set up the accecories that you may buy according to the item bought
+
+    // find the images? - suggest on discord.
+
+    // make sure the add to cart button works, and then create a button where you can view the items.
+}
